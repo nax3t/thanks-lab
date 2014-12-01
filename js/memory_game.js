a1ff427892d384bbd2c584a80806706fcdb212b6
nax3t <iantskon@gmail.com>
2014-11-30 19:24:33 -0600

    Start building memory game

---
+++ js/memory_game.js
@@ -0,0 +1,5 @@
+var memory_array = ['Cat','Cat','Dog','Dog','Hippo','Hippo','Lion','Lion','Giraffe','Giraffe','Alligator','Alligator','Snake','Snake','Spider','Spider','Iguana','Iguana','Dinosaur','Dinosaur','Whale','Whale','Shark','Shark','Monkey','Monkey'],
+memory_values = [],
+memory_tile_ids = [],
+tiles_flipped = 0;
+
